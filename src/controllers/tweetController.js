fdfea58155a955bbeb3bac6dfa558d59f0fc23c2
Niranjan Reddy <niranjanreddyvakareddy999@gmail.com>
2025-08-12 19:45:04 +0530

    controller layer add

---
+++ src/controllers/tweetController.js
@@ -0,0 +1,20 @@
+export const getTweet = (req,res)=>{
+    return res.json({
+        message : "common tweet"
+    })
+}
+
+export const getTweetById = (req,res)=>{
+    return res.json({
+        message : "this is tweet_id route",
+        id : req.params.id
+    })
+}
+
+export const createTweet = (req, res)=>{
+    return res.json({
+        message : "tweet created successfuly",
+        body : req.body
+    })
+
+}
--- src/routes/v1/tweet.js
+++ src/routes/v1/tweet.js
@@ -1,18 +1,12 @@
 import express from 'express'
+import { getTweet, getTweetById } from '../../controllers/tweetController.js';
 
 const router = express.Router();
 
-router.get('/', (req,res)=>{
-    return res.json({
-        message : "common tweet"
-    })
-})
+router.get('/', getTweet);
 
-router.get('/:id',(req,res)=>{
-    return res.json({
-        message : "this is tweet_id route",
-        id : req.params.id
-    })
-})
+router.get('/:id', getTweetById);
+
+
 
 export default router;
